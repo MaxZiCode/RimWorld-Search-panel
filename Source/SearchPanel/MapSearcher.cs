eddbe347d650ee1bf65637ccc168da67937038de
Maxim Stepul <maxzicode@gmail.com>
2020-07-24 14:37:17 +0300

    Fix disptaying cells.

--- Source/SearchPanel/MapSearcher.cs
+++ Source/SearchPanel/MapSearcher.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-
+using UnityEngine;
 using Verse;
 
 namespace SearchPanel
@@ -17,19 +18,39 @@ namespace SearchPanel
             var notFoggedCells = map.AllCells.Where(cell => !map.fogGrid.IsFogged(cell));
             var items = from cell in notFoggedCells
                         from thing in map.thingGrid.ThingsListAtFast(cell)
-                        group thing by (thing.def, thing.Stuff)
+                        group (thing, cell) by (thing.def, thing.Stuff)
 
                         into grouped
-                        select new SearchItem(grouped.Key.def, grouped.Select(t => t.Position), grouped.Count(), grouped.Key.Stuff);
+                        let def = grouped.Key.def
+                        let cells = grouped.Select(g => g.cell)
+                        let distinctThings = grouped.Select(g => g.thing).Distinct()
+                        let stuff = grouped.Key.Stuff
+                        select new SearchItem(def, cells, distinctThings.Count(), stuff);
 
             var terrains = from cell in notFoggedCells
                            let terrain = cell.GetTerrain(map)
-                           group (terrain, cell) by terrain
+                           group cell by terrain
 
                            into grouped
-                           select new SearchItem(grouped.Key, grouped.Select(g => g.cell), grouped.Count());
+                           let def = grouped.Key
+                           let cells = grouped
+                           select new SearchItem(def, cells, cells.Count());
 
             return items.Concat(terrains).OrderBy(item => item.Label);
+        }
+
+        public static IEnumerable<IntVec3> GetCells(Thing thing)
+        {
+            //if (thing.CustomRectForSelector != null)
+            //{
+            //    foreach (var cell in thing.CustomRectForSelector)
+            //        yield return cell;
+            //}    
+            //else
+            //{
+            //    yield return thing.Position;
+            //}
+            return new CellRect(thing.Position.x - thing.RotatedSize.x / 2, thing.Position.z - thing.RotatedSize.z / 2, thing.RotatedSize.x, thing.RotatedSize.z);
         }
     }
 }
